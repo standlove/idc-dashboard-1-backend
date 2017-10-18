188b00a73ecd8dcb0f37f3df6eb1f350392dfc02
stand4love@163.com <stand4love@163.com>
2017-10-18 14:20:54 +0800

    dd

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 module.exports = app; // for testing
 
 var config = {
